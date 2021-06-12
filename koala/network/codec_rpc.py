619c8b47af437e9121807802e170c3e8940f6c24
egmkang wang <egmkang@outlook.com>
2021-06-12 17:27:11 +0800

    1. 增加对pypy的支持

--- koala/network/codec_rpc.py
+++ koala/network/codec_rpc.py
@@ -1,4 +1,4 @@
-import orjson
+import json
 from koala.network.buffer import Buffer
 from koala.network.codec import Codec
 from koala.network.constant import *
@@ -6,11 +6,10 @@ from koala.message import *
 from koala.message.base import find_model, SimpleMessage
 from koala.logger import logger
 
-
 KOLA_MAGIC = "KOLA".encode()
-
-
 Message = Tuple[SimpleMessage, bytes]
+__json_dumps = json.dumps
+__json_loads = lambda b: json.loads(b.decode())
 
 
 # 4字节magic `KOLA`
@@ -23,23 +22,32 @@ class CodecRpc(Codec):
 
     def __init__(self):
         super(CodecRpc, self).__init__(CODEC_RPC)
+        try:
+            global __json_dumps, __json_loads
+            import orjson
+            __json_loads = orjson.loads
+            __json_dumps = orjson.dumps
+        except:
+            pass
 
     @classmethod
     def _encode_meta(cls, o: SimpleMessage) -> bytes:
         # 1字节长度
         # N字节MessageName
         # M字节json
+        global __json_dumps
         name: str = o.__class__.__qualname__
-        json_data: bytes = cast(bytes, orjson.dumps(o.to_dict()))
+        json_data: bytes = cast(bytes, __json_dumps(o.to_dict()))
         return b"".join((int.to_bytes(len(name), 1, 'little'), name.encode(), json_data))
 
     @classmethod
     def _decode_meta(cls, array: bytes) -> Optional[SimpleMessage]:
+        global __json_loads
         name_length = array[0]
         name = array[1: name_length + 1].decode()
         model = find_model(name)
         if model is not None:
-            json = orjson.loads(array[name_length+1:])
+            json = __json_loads(array[name_length + 1:])
             return model.from_dict(json)
         return None
 
